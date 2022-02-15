fa2f9654efca478cd2929c0b458ad47f2052a692
Vinicius Nordi <viniciusnordiesperanca@gmail.com>
2022-02-15 12:19:13 -0300

    Adjusting states and events

--- internal/workflows/shipmentwf/run_shp_wf.go
+++ internal/workflows/shipmentwf/run_shp_wf.go
@@ -17,23 +17,71 @@ func RunShpWf(ctx workflow.Context) (err error) {
 	}
 	ctx = workflow.WithActivityOptions(ctx, ao)
 
-	initShpWfEvent := ""
-	if err = workflow.ExecuteChildWorkflow(ctx, InitShpWf).Get(ctx, &initShpWfEvent); err != nil {
+	currentState := "init"
+	stateHistory := []string{currentState}
+
+	eventHistory := []string{}
+	currentEvent := ""
+
+	if err = workflow.ExecuteChildWorkflow(ctx, ActivateStoryWf).Get(ctx, nil); err != nil {
 		return
 	}
 
-	if initShpWfEvent != "PBQ" {
+	if err = workflow.ExecuteActivity(ctx, InitTeamWf).Get(ctx, nil); err != nil {
+		return
+	}
+
+	currentState = "ssp.approving"
+	stateHistory = append(stateHistory, currentState)
+
+	currentEvent = "PBQ"
+	eventHistory = append(eventHistory, currentEvent)
+
+	if currentEvent != "PBQ" {
 		err = fmt.Errorf("something went wrong running workflow. expected PBQ event")
 		return
 	}
 
-	sspApprovingWfEvent := ""
-	if err = workflow.ExecuteChildWorkflow(ctx, SspApprovingWf).Get(ctx, &sspApprovingWfEvent); err != nil {
-		return
+	if err = workflow.ExecuteChildWorkflow(ctx, ValidateRequestWf).Get(ctx, nil); err != nil {
+		currentEvent = "PBJ"
+		currentState = "fail"
+
+	} else {
+		currentEvent = "PBA"
+		currentState = "carrier.booking.send"
 	}
 
-	if sspApprovingWfEvent == "PBJ" {
-		if err = workflow.ExecuteChildWorkflow(ctx, FinishShpWf, sspApprovingWfEvent).Get(ctx, nil); err != nil {
+	stateHistory = append(stateHistory, currentState)
+	eventHistory = append(eventHistory, currentEvent)
+
+	if currentEvent == "PBJ" {
+		if err = workflow.ExecuteChildWorkflow(ctx, FinishShpWf, currentEvent).Get(ctx, nil); err != nil {
+			return
+		}
+	}
+
+	if err = workflow.ExecuteChildWorkflow(ctx, SendBookingWf).Get(ctx, nil); err != nil {
+		currentEvent = "BKJ"
+		currentState = "fail"
+
+		stateHistory = append(stateHistory, currentState)
+		eventHistory = append(eventHistory, currentEvent)
+	} else {
+		currentEvent = "BKQ"
+		currentState = "carrier.booking.sent"
+
+		stateHistory = append(stateHistory, currentState)
+		eventHistory = append(eventHistory, currentEvent)
+
+		currentEvent = "BKA"
+		currentState = "carrier.booking.approved"
+
+		stateHistory = append(stateHistory, currentState)
+		eventHistory = append(eventHistory, currentEvent)
+	}
+
+	if currentEvent == "BKJ" {
+		if err = workflow.ExecuteChildWorkflow(ctx, FinishShpWf, currentEvent).Get(ctx, nil); err != nil {
 			return
 		}
 	}
--- internal/workflows/shipmentwf/wfs.go
+++ internal/workflows/shipmentwf/wfs.go
@@ -4,12 +4,11 @@ import "go.temporal.io/sdk/worker"
 
 func RegisterWorkflows(w worker.Worker) {
 	w.RegisterWorkflow(RunShpWf)
-	w.RegisterWorkflow(InitShpWf)
 	w.RegisterWorkflow(InitTeamWf)
 	w.RegisterWorkflow(ActivateStoryWf)
-	w.RegisterWorkflow(SspApprovingWf)
 	w.RegisterWorkflow(ValidateRequestWf)
 	w.RegisterWorkflow(FinishShpWf)
 	w.RegisterWorkflow(DeactivateTeamWf)
 	w.RegisterWorkflow(InactivateHistoryWf)
+	w.RegisterWorkflow(SendBookingWf)
 }
